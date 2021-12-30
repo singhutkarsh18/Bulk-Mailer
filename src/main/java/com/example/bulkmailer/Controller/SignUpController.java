37649abcdd0248b06ebd636b72deb6758030d24b
Utkarsh <utkarshsingh100.us@gmail.com>
2021-12-30 19:54:37 +0530

    Fixed bugs

--- src/main/java/com/example/bulkmailer/Controller/SignUpController.java
+++ src/main/java/com/example/bulkmailer/Controller/SignUpController.java
@@ -1,29 +1,20 @@
 package com.example.bulkmailer.Controller;
 
 import com.example.bulkmailer.Entities.AppUser;
-import com.example.bulkmailer.Entities.DTOs.GoogleToken;
+import com.example.bulkmailer.Entities.DTOs.GoogleRequest;
 import com.example.bulkmailer.Entities.DTOs.OTP;
 import com.example.bulkmailer.Entities.DTOs.PasswordDto;
 import com.example.bulkmailer.Entities.RegistrationRequest;
 import com.example.bulkmailer.Repository.UserRepository;
 import com.example.bulkmailer.Services.RegisterService;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.Map;
 
 @RestController @AllArgsConstructor
@@ -130,35 +121,36 @@ public class SignUpController {
         }
     }
     @PostMapping("/google")
-    public ResponseEntity<?> googleSignIn(@RequestBody GoogleToken token) throws GeneralSecurityException, IOException {
-        String CLIENT_ID="852195797172-d0qq3vi9erb2ep1ill5eilc65mdvmah9.apps.googleusercontent.com";
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(CLIENT_ID))
-                .build();
-
-        String idTokenString=token.getToken();
-        GoogleIdToken idToken = verifier.verify(idTokenString);
-        if (idToken != null) {
-            Payload payload = idToken.getPayload();
-
-            String userId = payload.getSubject();
-            System.out.println("User ID: " + userId);
-
-            String email = payload.getEmail();
-            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-            String name = (String) payload.get("name");
-
-            if(!userRepository.findByUsername(email).isPresent())
-                userRepository.save(new AppUser(name,email,null,0));
-            if(emailVerified)
+    public ResponseEntity<?> googleSignIn(@RequestBody GoogleRequest googleRequest) {
+//        String CLIENT_ID="852195797172-d0qq3vi9erb2ep1ill5eilc65mdvmah9.apps.googleusercontent.com";
+//        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+//                .setAudience(Collections.singletonList(CLIENT_ID))
+//                .build();
+//
+//        String idTokenString=token.getToken();
+//        GoogleIdToken idToken = verifier.verify(idTokenString);
+//        if (idToken != null) {
+//            Payload payload = idToken.getPayload();
+//
+//            String userId = payload.getSubject();
+//            System.out.println("User ID: " + userId);
+//
+//            String email = payload.getEmail();
+//            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+//            String name = (String) payload.get("name");
+//
+            if(!userRepository.findByUsername(googleRequest.getEmail()).isPresent())
+                userRepository.save(new AppUser(googleRequest.getName(),googleRequest.getEmail(),null,0));
                 return ResponseEntity.status(HttpStatus.CREATED).build();
-            else
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("email not verified");
+//            if(emailVerified)
+//            else
+//                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("email not verified");
+//
+//        } else {
+//            System.out.println("Invalid ID token.");
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid ID token.");
+//        }
 
-        } else {
-            System.out.println("Invalid ID token.");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid ID token.");
-        }
     }
     @GetMapping("/hello")
     public String hello()
--- src/main/java/com/example/bulkmailer/Entities/DTOs/GoogleRequest.java
+++ src/main/java/com/example/bulkmailer/Entities/DTOs/GoogleRequest.java
@@ -5,5 +5,6 @@ import lombok.Setter;
 
 @Getter@Setter
 public class GoogleRequest {
-    ;
+    String name;
+    String email;
 }
