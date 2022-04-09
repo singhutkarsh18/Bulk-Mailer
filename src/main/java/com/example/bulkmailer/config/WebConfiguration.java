7a33d8c529c6555f1a15abbae3108790a2a08559
Utkarsh <utkarshsingh100.us@gmail.com>
2022-04-09 22:55:08 +0530

    Fixed cors bug

--- src/main/java/com/example/bulkmailer/config/WebConfiguration.java
+++ src/main/java/com/example/bulkmailer/config/WebConfiguration.java
@@ -1,10 +1,8 @@
 package com.example.bulkmailer.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -20,9 +18,21 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://chitthi.netlify.app")
+                .allowedOriginPatterns("http://localhost:3000/**")
+                .allowedOriginPatterns("https://chitthi.netlify.app/**")
                 .allowedMethods("POST", "GET")
-                //.allowedHeaders("header1", "header2", "header3")
-                //.exposedHeaders("header1", "header2")
-                .allowCredentials(false).maxAge(3600);
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
