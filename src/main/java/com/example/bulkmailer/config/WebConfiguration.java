580f6ab977d1b2255ebf44cea593e62d0ca5409c
Utkarsh <utkarshsingh100.us@gmail.com>
2022-04-09 22:12:21 +0530

    Cors updated

--- src/main/java/com/example/bulkmailer/config/WebConfiguration.java
+++ src/main/java/com/example/bulkmailer/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.bulkmailer.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,5 +14,14 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/static/**")
                 .addResourceLocations("file:///"+System.getProperty("user.dir")+"/target/classes/uploads");
+    }
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET")
+                //.allowedHeaders("header1", "header2", "header3")
+                //.exposedHeaders("header1", "header2")
+                .allowCredentials(true).maxAge(3600);
     }
 }
